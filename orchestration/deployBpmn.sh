4edce5972e5344b5ece8fc4e6a448050067f5007
somanath21 <104554892+somanath21@users.noreply.github.com>
2023-07-21 13:50:53 +0530

    upload bpmn in CircleCI (#131)
    Co-authored-by: Somanath Hugar <somanathhugar@fynarfin.io>

---
+++ orchestration/deployBpmn.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+HOST="https://zeebeops.sandbox.fynarfin.io/zeebe/upload"
+deploy(){
+    cmd="curl --insecure --location --request POST $HOST \
+    --header 'Platform-TenantId: gorilla' \
+    --form 'file=@\"$PWD/$1\"'"
+    echo $cmd
+    eval $cmd 
+    #If curl response is not 200 it should fail the eval cmd
+    
+    cmd="curl --insecure --location --request POST $HOST \
+    --header 'Platform-TenantId: rhino' \
+    --form 'file=@\"$PWD/$1\"'"
+    echo $cmd
+    eval $cmd
+    #If curl response is not 200 it should fail the eval cmd
+}
+
+LOC=orchestration/feel/*.bpmn
+for f in $LOC; do
+    deploy $f
+done
+
+LOC2=orchestration/feel/example/*.bpmn
+for f in $LOC2; do
+    deploy $f
+done
