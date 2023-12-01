5dd4d3daa71b3acb0d7c9e0180c28da4c57e1cda
logoutdhaval <dhaval@fynarfin.io>
2023-12-01 16:07:52 +0530

    [PHEE-499] Worker DFSPIDs in inbound-ams-mifo.bpmn workflow is not changing while uploading

--- orchestration/deployBpmn.sh
+++ orchestration/deployBpmn.sh
@@ -4,6 +4,13 @@ deploy(){
     cmd="curl --insecure --location --request POST $HOST \
     --header 'Platform-TenantId: gorilla' \
     --form 'file=@\"$PWD/$1\"'"
+        echo "print 1 $1"
+    if [ "$1" = "orchestration/feel/inbound_transfer-mifos-DFSPID.bpmn" ]; then
+      sed "s/account-identifier-DFSPID/account-identifier-gorilla/g; s/payee-loan-transfer-DFSPID/payee-loan-transfer-gorilla/g;s/payee-deposit-transfer-DFSPID/payee-deposit-transfer-gorilla/g;" $PWD/$1 > temp.bpmn
+      cmd="curl --insecure --location --request POST $HOST \
+      --header 'Platform-TenantId: gorilla' \
+      --form 'file=@\"$PWD/temp.bpmn\"'"
+    fi
     echo $cmd
     eval $cmd 
     #If curl response is not 200 it should fail the eval cmd
@@ -11,6 +18,12 @@ deploy(){
     cmd="curl --insecure --location --request POST $HOST \
     --header 'Platform-TenantId: rhino' \
     --form 'file=@\"$PWD/$1\"'"
+    if [ "$1" = "orchestration/feel/inbound_transfer-mifos-DFSPID.bpmn" ]; then
+      sed "s/account-identifier-DFSPID/account-identifier-rhino/g; s/payee-loan-transfer-DFSPID/payee-loan-transfer-rhino/g;s/payee-deposit-transfer-DFSPID/payee-deposit-transfer-rhino/g;" $PWD/$1 > temp.bpmn
+      cmd="curl --insecure --location --request POST $HOST \
+      --header 'Platform-TenantId: gorilla' \
+      --form 'file=@\"$PWD/temp.bpmn\"'"
+    fi
     echo $cmd
     eval $cmd
     #If curl response is not 200 it should fail the eval cmd
